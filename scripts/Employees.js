4b6dfb7caf3b3a7fb171153cbc24a6de5666f151
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-21 14:29:47 -0500

    Add event listeners

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,16 +1,39 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const clickedItem = clickEvent.target
+        if (clickedItem.dataset.type === "employee") {
+            let counter = 0
+            const orders = getOrders()
+
+            for (const order of orders) {
+                if (order.employeeId === parseInt(clickedItem.dataset.id)) {
+                    counter++
+                }
+            }
+            window.alert(`${clickedItem.dataset.name} sold ${counter} objects`)
+        }
+    }
+)
 
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `<li 
+                    data-type="employee" 
+                    data-id="${employee.id}" 
+                    data-name="${employee.name}"
+                    >${employee.name}
+                    </li>`
     }
 
     html += "</ul>"
 
-    return htm
+    return html
 }
 
--- scripts/Products.js
+++ scripts/Products.js
@@ -2,11 +2,31 @@ import { getProducts } from "./database.js"
 
 const products = getProducts()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const clickedItem = clickEvent.target
+       
+        if (clickedItem.dataset.type === "product") {
+            const priceOfProduct = (parseFloat(clickedItem.dataset.price).toFixed(2))
+
+            window.alert(`${clickedItem.dataset.name} costs $${priceOfProduct}`)
+        }
+
+    }
+)
+
 export const Products = () => {
-    let html = "<ul>`
+    let html = `<ul>`
 
     for (const product of products) {
-        html += `<li>${product.title}</li>`
+        html += `<li 
+            data-type="product" 
+            data-id="${product.id}" 
+            data-price="${product.price}" 
+            data-name="${product.name}"
+            >${product.name}
+            </li>`
     }
 
     html += "</ul>"
